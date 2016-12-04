94e817f760444fdf2e1d301cfb92ce664ffb0864
McRage <hawk45@list.ru>
2016-12-04 10:16:12 +0200

    Divided proj2

---
+++ JinnSportsWEB/JinnSports.DAL/Repositories/EFUnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using JinnSports.DataAccessInterfaces;
+using JinnSports.DAL.EFContext;
+using JinnSports.Entities;
+
+namespace JinnSports.DAL.Repositories
+{
+    public class EFUnitOfWork : IUnitOfWork
+    {
+        private SportsContext db;
+        
+        public EFUnitOfWork()
+        {
+            db = new SportsContext();            
+        }
+
+        public IRepository<T> Set<T>() where T : class
+        {
+            return new BaseRepository<T>(db);
+        }
+
+        public void SaveChanges()
+        {
+            db.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }        
+    }
+}
---
+++ JinnSportsWEB/JinnSports.DataAccessInterfaces/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JinnSports.DataAccessInterfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> Set<T>() where T : class;
+        void SaveChanges();
+    }
+}
